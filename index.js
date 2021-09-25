1998584da088a5aeba11776e5d0e842529aaa7e7
Angelo <75602348+Lee-Seung-Wook@users.noreply.github.com>
2021-09-25 20:57:25 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// Filter-Section
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = $('.filterDiv');
+    // x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+}
+// Add show class to activated Filter-Div
+function AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+// Remove show class to not activated Filter-Div
+function RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+// Add active class to the current button
+var btnContainer = $('#myBtnContainer');
+// var btnContainer = document.getElementById("MyBtnContainer");
+var btns = $('.btn');
+// var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = $('.active');
+        // var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
