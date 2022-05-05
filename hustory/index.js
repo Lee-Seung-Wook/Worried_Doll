832633c1e5277ca2455b355a09a6953782bf5f56
Angelo <75602348+Lee-Seung-Wook@users.noreply.github.com>
2022-05-05 13:56:02 +0900

    Update index.js

--- hustory/index.js
+++ hustory/index.js
@@ -44,9 +44,8 @@ function GoToNext() {
         $('.btn_next').fadeOut(300);
         $('.slick').slick('slickGoTo', slideindex);
         typing_set();
-        console.log(slideindex);
     }, 500);
-    if (slideindex == 16) {
+    if (slideindex == 15) {
         let AD = $('#KakaoAD');
         let KakaoAD1 = '<ins class="kakao_ad_area" style="display:none;" data-ad-unit="DAN-dk7u0oeA6UnqjuXL" data-ad-width="300" data-ad-height="250"></ins>';
         let KakaoAD2 = document.createElement('script');
@@ -152,10 +151,6 @@ function GoToResult() {
         let sn = ($('#input4').val() * 1) + ($('#input5').val() * 1) + ($('#input6').val() * 1) + ($('#input7').val() * 1)
         let tf = ($('#input8').val() * 1) + ($('#input9').val() * 1) + ($('#input10').val() * 1) + ($('#input11').val() * 1)
         let pj = ($('#input12').val() * 1) + ($('#input13').val() * 1) + ($('#input14').val() * 1) + ($('#input15').val() * 1)
-        console.log(ei);
-        console.log(sn);
-        console.log(tf);
-        console.log(pj);
         let array = [ei, sn, tf, pj]
         for (i = 0; i < array.length; i++) {
             if (array[i] == 0) {
